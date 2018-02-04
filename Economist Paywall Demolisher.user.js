dcedb82fb767371646a5519d0133b3d5812e085d
xnmll <xnmlllcy@gmail.com>
2018-02-04 10:43:17 +0800

    add economist paywall demolisher

---
+++ Economist Paywall Demolisher.user.js
@@ -0,0 +1,9 @@
+// ==UserScript==
+// @name        Economist Paywall Demolisher
+// @namespace   Abdussamad
+// @description Removes the limit on the number of articles you can view for free on The Economist's site.
+// @include     http://*.economist.com/*
+// @version     0.1
+// @grant       none
+// ==/UserScript==
+document.cookie="ec_limit=allow"
